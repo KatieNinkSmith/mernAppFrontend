42efc2ec602e6ce30cb7d43c2dae97b3e8356e91
Katie Nink Smith <katiej.smith@outlook.com>
2025-01-08 09:18:58 -0700

    sign up form created

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -10,14 +10,71 @@ function SignUpForm() {
   const [error, setError] = useState("");
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.value]: e.target.value });
+    setFormData({ ...formData, [e.target.name]: e.target.value });
     setError("");
   };
 
   const handleSubmit = (e) => {
+    e.preventDefault();
     console.log(formData);
   };
-  return <div>Sign Up</div>;
+
+  return (
+    <>
+      <h1>Sign Up to start using your own Calendar</h1>
+      <div>
+        <form autoComplete="off" onSubmit={handleSubmit}>
+          <label>Display name:</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="Display Name"
+            required
+          />
+          <br />
+          <label>Email address (needs to be unique)</label>
+          <br />
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="email address will be your login"
+            required
+          />
+          <br />
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            placeholder="Display password"
+            required
+          />
+          <br />
+          <label>Confirm Password</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            placeholder="Must match password"
+            required
+          />
+          <br />
+          <button
+            type="submit"
+            disabled={formData.password !== formData.confirmPassword}
+          >
+            SIGN UP now
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default SignUpForm;
