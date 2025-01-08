79e71218c3bd1ea16a989b03957f9a70c3841009
Katie Nink Smith <katiej.smith@outlook.com>
2025-01-08 11:27:39 -0700

    those pesky S's

--- src/utilities/users-services.js
+++ src/utilities/users-services.js
@@ -1,14 +1,15 @@
-import * as usersAPI from "./users-api";
+import * as userAPI from "./users-api";
 
 // I need to pass in userData because this is attempting to add a new user to the database
 export async function signUp(userData) {
   // delegate the network request code to the users-api.js API module
   // which will ultimately return JWT
   const token = await userAPI.signUp(userData);
+  console.log(token);
   // for now, we will console.log the token to see that it exists and return the name and email that was sent to us we will also eventually save the token in localStorage
   // localStorage.setItem("token", token)
   return {
-    name: "Your mom",
-    email: "email@yourmom.com",
+    name: userData.name,
+    email: userDate.email,
   };
 }
