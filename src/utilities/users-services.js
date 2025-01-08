1092f3be01cb83903555b8208ebf4d5064100b34
Katie Nink Smith <katiej.smith@outlook.com>
2025-01-08 09:44:31 -0700

    working on auth

---
+++ src/utilities/users-services.js
@@ -0,0 +1,7 @@
+// I need to pass in userData because this is attempting to add a new user to the database
+export async function signUp(userData) {
+  return {
+    name: "Your mom",
+    email: "email@yourmom.com",
+  };
+}
